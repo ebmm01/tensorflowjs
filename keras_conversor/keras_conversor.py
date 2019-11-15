54b497d62d885e00aebf70b43e78b75ce42ec711
Elias Bernardo <ebmm01@gmail.com>
2019-11-15 18:38:30 -0300

    Adding Mobile net to keras_conversor script and tensorflow to requirements.txt

--- keras_conversor/keras_conversor.py
+++ keras_conversor/keras_conversor.py
@@ -1,5 +1,12 @@
 import tensorflowjs as tfjs
 import keras
+import tensorflow as tf
 
-vgg16 = keras.applications.vgg16.VGG16()
-tfjs.converters.save_keras_model(vgg16, './VGG16')
+tf.compat.v1.disable_eager_execution()
+
+
+#vgg16 = keras.applications.vgg16.VGG16()
+#tfjs.converters.save_keras_model(vgg16, './VGG16')
+
+mobilenet = keras.applications.mobilenet_v2.MobileNetV2()
+tfjs.converters.save_keras_model(mobilenet, './mobilenet')
